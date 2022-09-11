1a46d090e5a6c612b42a2fe87d355062207e1ce0
Xisurthros <Jacob_Hobbs548@yahoo.com>
2022-09-11 15:13:31 -0500

    rename main __main__

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,11 +1,15 @@
 from life360 import Life360
+
 def main():
+
     function_dict = {'me': life360.me, 'circles': life360.circles,
                     'code': life360.code, 'messages': life360.messages,
                     'circle_data': life360.circle_data, 'history': life360.history,
                     'emergency_contacts': life360.emergency_contacts, 'set_circle': life360.set_circle, 
                     'help': life360.help}
+
     while True:
+        
         user_input = input('Enter: ')
         user_input.lower()
         if user_input in function_dict:
